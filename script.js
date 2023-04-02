ef74059ed118d452ddf2174ec0a338ab802f71ce
ArsenYavorskyi <ars.yar24@gmail.com>
2023-04-02 22:21:12 +0300

    added loader

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const elements = {
     $quoteAuthor: {},
     $twitterBtn: {},
     $newQuoteBtn: {},
+    $loader: {},
 };
 
 function getDomElements() {
@@ -13,14 +14,27 @@ function getDomElements() {
     elements.$quoteAuthor = document.querySelector('.author');
     elements.$twitterBtn = document.querySelector('.btn__twitter');
     elements.$newQuoteBtn = document.querySelector('.btn__new-quote');
+    elements.$loader = document.querySelector('.loader');
+}
+
+// loader functions to show and hide loader
+
+function loading() {
+    elements.$loader.hidden = false;
+    elements.$quoteContainer.hidden = true;
+}
+
+function complete() {
+    elements.$quoteContainer.hidden = false;
+    elements.$loader.hidden = true;
 }
 
 // Get quote from API
 let quotesList = [];
 
-
 // Get quote from quotesList array and display it
 function getRandomQuote() {
+    loading();
     const quote = quotesList[Math.floor(Math.random() * quotesList.length)];
     if (!quote.author) {
         elements.$quoteAuthor.textContent = 'Unknown';
@@ -33,10 +47,12 @@ function getRandomQuote() {
         elements.$quoteText.classList.remove('long-quote');
     }
     elements.$quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get quotes from API for the first time and then get random quote from quotesList array and display it
 async function displayQuote() {
+    loading();
     try {
         const apiUrl = 'https://type.fit/api/quotes';
         const response = await fetch(apiUrl);
