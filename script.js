a20f03519ad251e37bc9c643d75f03b10d828271
ArsenYavorskyi <ars.yar24@gmail.com>
2023-04-02 20:24:42 +0300

    added for twitter and new-quote buttons

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
-// Get quote from API
-function getQuote(quotesArr) {
-    const randomIndex = Math.floor(Math.random() * quotesArr.length);
-    return quotesArr[randomIndex];
+// get Dom elements to work with
+const elements = {
+    $quoteContainer: {},
+    $quoteText: {},
+    $quoteAuthor: {},
+    $twitterBtn: {},
+    $newQuoteBtn: {},
+};
+
+function getDomElements() {
+    elements.$quoteContainer = document.querySelector('.quote__container');
+    elements.$quoteText = document.querySelector('.quote');
+    elements.$quoteAuthor = document.querySelector('.author');
+    elements.$twitterBtn = document.querySelector('.btn__twitter');
+    elements.$newQuoteBtn = document.querySelector('.btn__new-quote');
 }
 
-async function getQuotes() {
+// Get quote from API
+let quotesList = [];
+
+
+// Get quote from quotesList array and display it
+function getRandomQuote() {
+    const quote = quotesList[Math.floor(Math.random() * quotesList.length)];
+    if (!quote.author) {
+        elements.$quoteAuthor.textContent = 'Unknown';
+    } else {
+        elements.$quoteAuthor.textContent = quote.author;
+    };
+    if (quote.text.length > 120) {
+        elements.$quoteText.classList.add('long-quote');
+    } else {
+        elements.$quoteText.classList.remove('long-quote');
+    }
+    elements.$quoteText.textContent = quote.text;
+}
+
+// Get quotes from API for the first time and then get random quote from quotesList array and display it
+async function displayQuote() {
     try {
         const apiUrl = 'https://type.fit/api/quotes';
         const response = await fetch(apiUrl);
-        const quotes = await response.json();
-        const oneQuote = getQuote(quotes);
-        console.log(oneQuote);
+        quotesList = await response.json();
+        getRandomQuote();
     } catch (error) {
-        alert(error)
+        alert(error);
     }
 }
 
-getQuotes();
+// twitter button functionality
+function tweetQuote() {
+    const quote = elements.$quoteText.textContent;
+    const author = elements.$quoteAuthor.textContent;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+getDomElements();
+displayQuote();
+elements.$newQuoteBtn.addEventListener('click', getRandomQuote);
+elements.$twitterBtn.addEventListener('click', tweetQuote);
+
+
